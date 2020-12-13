4cd58e8431f9b324ad33fbb246775a4565b71b16
DAigory <mrcalibus@gmail.com>
2020-12-13 19:32:55 +0700

    init

---
+++ Source/decodingProfile/decodingProfile.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "decodingProfile.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, decodingProfile, "decodingProfile" );
